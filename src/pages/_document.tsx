f226a0318c9d4563e9caca8932a8946942ba5ccf
berkanbasaran05 <berkanbasaran05@gmail.com>
2023-08-17 20:59:24 +0300

    last

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+       <Head>
+        <title>Eda-Yasin</title>
+        <link rel="icon" href="/favicon.svg" />
+        <meta name="robots" content="noindex,nofollow" />
+        <meta name="viewport" content="width=device-width, maximum-scale=1" />
+      </Head>
       <body>
         <Main />
         <NextScript />
